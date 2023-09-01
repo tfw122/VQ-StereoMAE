89c0eb06b4443ddadd95a9792dbfef5f363b672a
Tia Warner <tfw122@ic.ac.uk>
2023-09-01 16:50:09 +0100

    attempt-1

--- run_vqkd_training.py
+++ run_vqkd_training.py
@@ -251,6 +251,8 @@ def main(args):
 
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
         model_without_ddp = model.module
+    else:
+        pass
 
     print("Use step level LR & WD scheduler!")
     lr_schedule_values = utils.cosine_scheduler(
