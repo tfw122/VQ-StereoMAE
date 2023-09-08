9e968c15b74284bd443c8da0c5d5b22ccbc026a5
Tia Warner <tfw122@ic.ac.uk>
2023-09-08 16:22:56 +0100

    debug

--- vqkd/modeling_finetune.py
+++ vqkd/modeling_finetune.py
@@ -158,7 +158,7 @@ class Attention(nn.Module):
         return x
 
 
-class ABlock(nn.Module):
+class Block(nn.Module):
 
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., init_values=None, act_layer=nn.GELU, norm_layer=nn.LayerNorm,
