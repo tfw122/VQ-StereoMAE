073a6827a7c1829bab9942a391e3079a04f7434a
Tia Warner <tfw122@ic.ac.uk>
2023-09-08 17:06:30 +0100

    debug

--- models/masked_vision_model.py
+++ models/masked_vision_model.py
@@ -78,7 +78,7 @@ class MaskedImageAutoEncoder(BaseModel):
         self.blocks = nn.ModuleList([
             Block(
                 dim=self.model_config.image_encoder.embed_dim, num_heads = self.model_config.image_encoder.num_heads, mlp_ratio=self.model_config.image_encoder.mlp_ratio,
-                drop=self.model_config.drop_rate, attn_drop=self.model_config.attn_drop_rate, drop_path=dpr[i], norm_layer=self.model_config.norm_layer,
+                drop=self.model_config.drop_rate, attn_drop=self.model_config.attn_drop_rate, drop_path=dpr[i],
                 attn_head_dim=self.model_config.attn_head_dim, act_layer=self.model_config.act_layer
             )
             for i in range(self.model_config.image_encoder.depth)])
